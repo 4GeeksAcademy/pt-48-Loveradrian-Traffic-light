56740c5029ccd0d7b855543a6c15333d44315305
Adrian Lovera <105324331+loveradrian28@users.noreply.github.com>
2023-11-09 10:51:27 +0000

    Traffic light

---
+++ Readme2.md
@@ -0,0 +1,3 @@
+Enlace con el ejercicio resuelto en un repl
+
+https://stackblitz.com/edit/stackblitz-starters-kfpove?file=src%2FApp.js
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,123 @@
 //import react into the bundle
-import React from "react";
+
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+import React, { useState, useEffect } from 'react';
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+const RedLight = ({ onOf }) => {
+  return (
+    <>
+      {
+        <div
+          className="red-light__container"
+          style={
+            onOf
+              ? {
+                  backgroundColor: 'red',
+                  boxShadow: '0 0 50px #ff2100',
+                }
+              : {}
+          }
+        ></div>
+      }
+    </>
+  );
+};
+const OrangeLight = ({ onOf }) => {
+  return (
+    <>
+      {
+        <div
+          className="orange-light__container"
+          style={
+            onOf
+              ? {
+                  backgroundColor: 'orange',
+                  boxShadow: '0 0 50px #ffe900',
+                }
+              : {}
+          }
+        ></div>
+      }
+    </>
+  );
+};
+const GreenLight = ({ onOf }) => {
+  return (
+    <>
+      {
+        <div
+          className="green-light__container"
+          style={
+            onOf
+              ? {
+                  backgroundColor: 'green',
+                  boxShadow: '0 0 50px #00ff42',
+                }
+              : {}
+          }
+        ></div>
+      }
+    </>
+  );
+};
+
+const TrafficLight = () => {
+  const [redLight, setRedLight] = useState(false);
+  const [orangeLight, setOrangeLight] = useState(false);
+  const [greenLight, setGreenLight] = useState(false);
+
+  const setRed = () => {
+    setRedLight(true);
+    console.clear();
+    console.log('soy rojo durante 3 segundos');
+    setTimeout(() => {
+      setOrange();
+    }, 3000);
+  };
+  const setOrange = () => {
+    setRedLight(false);
+    setOrangeLight(true);
+    console.log('soy naranja durante 1 segundos');
+    setTimeout(() => {
+      setGreen();
+    }, 1000);
+  };
+  const setGreen = () => {
+    setOrangeLight(false);
+    setGreenLight(true);
+    console.log('soy verde durante 3 segundos');
+    console.log('Me reinicio en 3 segundos');
+    setTimeout(() => {
+      restartLight();
+    }, 3000);
+  };
+
+  const restartLight = () => {
+    setGreenLight(false);
+    setRed();
+  };
+
+  const startLight = () => {
+    setRed();
+  };
+  useEffect(() => startLight, []);
+
+  return (
+    <>
+      <div className="traffic-light__container">
+        {<RedLight onOf={redLight} />}
+        {<OrangeLight onOf={orangeLight} />}
+        {<GreenLight onOf={greenLight} />}
+      </div>
+    </>
+  );
+};
+
+export default function App() {
+  return <TrafficLight />;
+}
